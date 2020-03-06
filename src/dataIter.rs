608521a073c73482dcf3a7568de0fa1a85855206
Paul Kassianik <pkassianik@gmail.com>
2020-03-05 21:36:03 -0800

    feat/main: Added the ability to define layers from parameters

--- src/dataIter.rs
+++ src/dataIter.rs
@@ -1,55 +1,56 @@
 use std::fs::File;
 use std::iter::FromIterator;
 use byteorder::ReadBytesExt;
+use std::io::Empty;
 
-pub struct DataIter<'a> {
-	pub labelFile: &'a mut File,
-	pub imageFile: &'a mut File,
+pub struct DataIter {
+	pub label_file: File,
+	pub image_file: File,
 	pub rows: usize,
 	pub cols: usize,
 }
 
-impl DataIter<'_> {
-	pub fn new<'a>(labelFile: &'a mut File, imageFile: &'a mut File, rows: usize, cols: usize) -> DataIter<'a> {
+impl DataIter {
+	pub fn new(label_file: File, image_file: File, rows: usize, cols: usize) -> DataIter {
 		DataIter {
-			labelFile,
-			imageFile,
+			label_file,
+			image_file,
 			rows,
 			cols
 		}
 	}
 }
 
-impl Iterator for DataIter<'_> {
+impl Iterator for DataIter {
 	type Item = (u8, Vec<u8>);
 
 	fn next(&mut self) -> Option<Self::Item> {
-		let label = match self.labelFile.read_u8() {
+		let label = match self.label_file.read_u8() {
 			Err(_) => return None,
 			Ok(l) => l,
 		};
 
 		let image: Vec<u8> = Vec::from_iter((0..(self.rows * self.cols)).map(|_| -> u8 {
 			// Have to flip bits because otherwise it will be white text on a dark background
-			self.imageFile.read_u8().unwrap() ^ 255
+			self.image_file.read_u8().unwrap() ^ 255
 		}));
 
 		Some((label, image))
 	}
 }
 
-pub struct BatchDataIter<'a> {
-	pub dataIter: DataIter<'a>,
+pub struct BatchDataIter {
+	pub data_iter: DataIter,
 	pub batch_size: usize,
 }
 
-impl Iterator for BatchDataIter<'_> {
+impl Iterator for BatchDataIter {
 	type Item = Vec<(u8, Vec<u8>)>;
 
 	fn next(&mut self) -> Option<Self::Item> {
 		let mut result_vec: Self::Item = vec![];
 		for _ in 0..self.batch_size {
-			match self.dataIter.next() {
+			match self.data_iter.next() {
 				None => break,
 				Some(v) => result_vec.push(v)
 			}
